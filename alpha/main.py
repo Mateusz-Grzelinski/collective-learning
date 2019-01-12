5ad51bee50f8a01d46c8f44f6802601de102519b
Mateusz Grzelinski <czubaka111.gmail.com>
2019-01-12 19:13:07 +0100

    VIP: reformat code
    Logic is broken, fix in following commit

--- alpha/main.py
+++ alpha/main.py
@@ -1,74 +1,85 @@
 # -*- coding: utf-8 -*-
 
 import networkx as nt
-import math
 import numpy as np
 import random
-p=20
-size=10
-n1=10
-n2=10
-group_size=n1*n2
 
-def zdobadz_wiedze(G,i,field):
-    x=random.randrange(size);
-    y=random.randrange(size);
-    if(field[x,y]>0):
-        nt.get_node_attributes(G,'agent')[i].knowledge[x,y]=field[x,y]-1
-        nt.get_node_attributes(G,'agent')[i].hypothesis[x,y]=field[x,y]-1
-        nt.get_node_attributes(G,'agent')[i].przypisanie=(x,y)
-    
+p = 20
+map_size = 10
+n1 = 10
+n2 = 10
 
-class agent:
-    def __init__(self,G):
-        self.przypisanie=None
-        self.knowledge=np.zeros((size,size),dtype=int)
-        self.hypothesis=np.zeros((size,size),dtype=int)
 
-def powieksz_wiedze(G,group_size,size):
-    for i in range(group_size):
-        if(nt.get_node_attributes(G,'agent')[i].przypisanie!=None):
-            indeks_sasiada=0
-            for j in range(size):
-                for k in range(size):
-                    while(nt.get_node_attributes(G,'agent')[i].knowledge[j,k]>0 and indeks_sasiada<len(list(nt.neighbors(G,i)))):
-                        if(nt.get_node_attributes(G,'agent')[list(nt.neighbors(G,i))[indeks_sasiada]].przypisanie==None):
-                            nt.get_node_attributes(G,'agent')[i].knowledge[j,k] = nt.get_node_attributes(G,'agent')[i].knowledge[j,k]-1
-                            nt.get_node_attributes(G,'agent')[list(nt.neighbors(G,i))[indeks_sasiada]].przypisanie=(i,k)
-                        indeks_sasiada=indeks_sasiada+1
-            for j in range(len(list(nt.neighbors(G,i)))):
-                nt.get_node_attributes(G,'agent')[list(nt.neighbors(G,i))[j]].knowledge
-                nt.get_node_attributes(G,'agent')[i].hypothesis=np.logical_or(nt.get_node_attributes(G,'agent')[i].knowledge,nt.get_node_attributes(G,'agent')[list(nt.neighbors(G,i))[j]].knowledge)
-    for i in range(group_size):
-        nt.get_node_attributes(G,'agent')[i].knowledge=nt.get_node_attributes(G,'agent')[i].hypothesis.copy()
-    sum=0
-    for i in range(group_size):
-        if(nt.get_node_attributes(G,'agent')[i].przypisanie!=None):
-            sum=sum+1
-    print(sum)
-    
+def get_knowledge(G, i, field):
+    x = random.randrange(map_size)
+    y = random.randrange(map_size)
+    attributes = G.nodes[i]
+    if (field[x, y] > 0):
+        attributes['knowledge'][x, y] = field[x, y] - 1
+        attributes['hypothesis'][x, y] = field[x, y] - 1
+        attributes['assigment'] = (x, y)
 
-G=nt.caveman_graph(n1,n2)
-field=np.zeros((size,size),dtype=int)
-for i in range(p):
-    x=random.randrange(size)
-    y=random.randrange(size)
-    field[x,y]=4
-valu={}
-for i in range(group_size):
-    valu[i]=agent(G)
-nt.set_node_attributes(G,valu,'agent')
-#print(type(G))
-#print(nt.neighbors(G,5))
-print(field)
-for j in range(7):
-    for i in range(group_size):
-        if (nt.get_node_attributes(G,'agent')[i].przypisanie==None):
-            zdobadz_wiedze(G,i,field)
-    #powiększanie wiedzy
-    sum=0
-    for i in range(group_size):
-        if(nt.get_node_attributes(G,'agent')[i].przypisanie!=None):
-            sum=sum+1
-    print(sum)
-    powieksz_wiedze(G,group_size,size)
+
+def share_knowledge(G, map_size):
+    for node, attributes in G.nodes(data=True):
+        if (attributes['assigment'] is not None):
+            nhood = G[i]
+            for j in range(map_size):
+                for k in range(map_size):
+                    for neighbour in nhood:
+                        if (attributes['knowledge'][j, k] > 0):
+                            break
+                        connected_node_attr = G.nodes[neighbour]
+                        if (connected_node_attr['assigment'] is None):
+                            attributes['knowledge'][j, k] = attributes['knowledge'][j, k] - 1
+                            connected_node_attr['assigment'] = (i, k)
+
+            for neighbour in nhood:
+                neigh_attr = G.nodes[neighbour]
+                neigh_attr['hypothesis'] = np.logical_or(
+                    attributes['knowledge'], G.nodes[neighbour]['knowledge']
+                )
+
+    for _, attributes in G.nodes(data=True):
+        attributes['knowledge'] = attributes['hypothesis'].copy()
+
+    # sum = 0
+    # for node, attributes in G.nodes(data=True):
+    #     if (attributes['assigment'] is not None):
+    #         sum = sum + 1
+    # print(sum)
+
+
+if __name__ == "__main__":
+    # base graph
+    G = nt.caveman_graph(n1, n2)
+
+    # setup initial map conditions
+    field = np.zeros((map_size, map_size), dtype=int)
+    for i in range(p):
+        x = random.randrange(map_size)
+        y = random.randrange(map_size)
+        field[x, y] = 4
+
+    # setup initial node attributes
+    for node, attributes in G.nodes(data=True):
+        attributes['assigment'] = None
+        attributes['knowledge'] = np.zeros((map_size, map_size), dtype=int)
+        attributes['hypothesis'] = np.zeros((map_size, map_size), dtype=int)
+
+    # print(type(G))
+    # print(nt.neighbors(G,5))
+    print(field)
+
+    number_of_iterations = 7
+    for _ in range(number_of_iterations):
+        for _, attributes in G.nodes(data=True):
+            if (attributes['assigment'] is None):
+                get_knowledge(G, i, field)
+        # powiększanie wiedzy
+        sum = 0
+        for _, attributes in G.nodes(data=True):
+            if (attributes['assigment'] is not None):
+                sum = sum + 1
+        print(sum)
+        share_knowledge(G, map_size)
