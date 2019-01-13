0649040ac3393e92ebf4a5c1f3c9f2954cf23453
Mateusz Grzelinski <czubaka111.gmail.com>
2019-01-13 09:39:54 +0100

    Format against yapf

--- alpha/main.py
+++ alpha/main.py
@@ -7,7 +7,7 @@ import numpy as np
 import random
 
 p = 50
-p_val=11
+p_val = 11
 map_size = 50
 n1 = 10
 n2 = 100
@@ -21,26 +21,26 @@ def get_knowledge(G, i, field):
         attributes['knowledge'][x, y] = field[x, y] - 1
         attributes['age_of_knowledge'] = 1
         attributes['assigment'] = (x, y)
-        field[x,y]=0
+        field[x, y] = 0
 
 
 def share_knowledge(G, map_size):
     for node, attributes in G.nodes(data=True):
-        if (attributes['assigment'] is not None): 
-            r=False
+        if (attributes['assigment'] is not None):
+            r = False
             for j in range(map_size):
                 for k in range(map_size):
                     if (attributes['knowledge'][j, k] > 0):
-                        nhood = set(nt.neighbors(G,node))
+                        nhood = set(nt.neighbors(G, node))
                         old_nhood = nhood.copy()
                         temp_age = attributes['age_of_knowledge']
-                        attributes['age_of_knowledge']=attributes['age_of_knowledge']+1
+                        attributes['age_of_knowledge'] = attributes['age_of_knowledge'] + 1
                         for i in range(temp_age):
-                            #print(old_nhood)
+                            # print(old_nhood)
                             for close_neighbour in old_nhood:
-                                nhood.update(set(nt.neighbors(G,close_neighbour)))
-                                #print(set(nt.neighbors(G,close_neighbour)))
-                            old_nhood=nhood.copy()
+                                nhood.update(set(nt.neighbors(G, close_neighbour)))
+                                # print(set(nt.neighbors(G,close_neighbour)))
+                            old_nhood = nhood.copy()
 #                        print(nhood)
 #                        print(temp_age)
 #                        print(node)
@@ -49,12 +49,12 @@ def share_knowledge(G, map_size):
                             if (connected_node_attr['assigment'] is None and attributes['knowledge'][j, k] > 0):
                                 attributes['knowledge'][j, k] = attributes['knowledge'][j, k] - 1
                                 connected_node_attr['assigment'] = (j, k)
-                        r=True
+                        r = True
                     if(r):
                         break
                 if(r):
                     break
-                    
+
 #            for neighbour in nhood:
 #                neigh_attr = G.nodes[neighbour]
 #                neigh_attr['age_of_knowledge'] = np.logical_or(
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     for i in range(p):
         x = random.randrange(map_size)
         y = random.randrange(map_size)
-        while field[x,y]>0:   
+        while field[x, y] > 0:
             x = random.randrange(map_size)
             y = random.randrange(map_size)
         field[x, y] = p_val
@@ -99,7 +99,7 @@ if __name__ == "__main__":
     for _ in range(number_of_iterations):
         for node, attributes in G.nodes(data=True):
             if (attributes['assigment'] is None):
-                #print(node)
+                # print(node)
                 get_knowledge(G, node, field)
         # powiększanie wiedzy
         sum = 0
@@ -109,7 +109,7 @@ if __name__ == "__main__":
         print("przed")
         print(sum)
         share_knowledge(G, map_size)
-        sum=0
+        sum = 0
         for _, attributes in G.nodes(data=True):
             if (attributes['assigment'] is not None):
                 sum = sum + 1
