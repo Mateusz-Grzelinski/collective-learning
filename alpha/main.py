db388b1a3760b7af90bc6f269339df0bb212ce40
Wilkkor <kornelwil@gmail.com>
2019-01-18 13:48:11 +0100

    Update main.py

--- alpha/main.py
+++ alpha/main.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 18 13:34:41 2019
+
+@author: Jan
+"""
+
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
 
 import networkx as nt
 import numpy as np
@@ -37,6 +48,7 @@ def share_knowledge(G):
                 # get neighbors of neighbors
                 for node in nhood.copy():
                     nhood.update(nt.neighbors(G, node))
+            #print(nhood);
 
             attributes['age_of_knowledge'] += 1
 
@@ -46,7 +58,8 @@ def share_knowledge(G):
             for neighbour in nhood:
                 connected_node_attr = G.nodes[neighbour]
                 if (connected_node_attr['assigment'] is None and map_knowledge > 0):
-                    attributes['knowledge'][index] = map_knowledge - 1
+                    map_knowledge=map_knowledge-1
+                    attributes['knowledge'][index] = map_knowledge
                     connected_node_attr['assigment'] = index
             break
 
